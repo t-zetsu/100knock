0d4fa33caae18e3958df1a3b1c2d874e84536359
t-zetsu <74550103+t-zetsu@users.noreply.github.com>
2021-05-13 17:58:27 +0900

    Delete 04.py

--- 04.py
+++
@@ -1,9 +0,0 @@
-str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.".strip(".").split()
-n = [1, 5, 6, 7, 8, 9, 15, 16, 19]
-d={}
-for i in range(len(str)):
-    if i+1 in n:
-        d[str[i][0]] = i+1
-    else:
-        d[str[i][0:2]] = i+1
-print(d)
