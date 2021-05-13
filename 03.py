ba94f071fc4586253b4a5319b5964148cecd9801
t-zetsu <74550103+t-zetsu@users.noreply.github.com>
2021-05-13 17:58:22 +0900

    Delete 03.py

--- 03.py
+++
@@ -1,7 +0,0 @@
-str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".split()
-
-n = []
-for i in range(len(str)):
-    n.append(len(str[i].strip(",.")))
-
-print(n)
