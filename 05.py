82e4d2ec420464e47b878a89df5cdd1dc1d7f5e1
t-zetsu <74550103+t-zetsu@users.noreply.github.com>
2021-05-13 17:58:34 +0900

    Delete 05.py

--- 05.py
+++
@@ -1,9 +0,0 @@
-str = "I am an NLPer"
-def ngram(str,n):
-    gram=[]
-    for i in range(len(str)-n+1):
-        gram.append(str[i:i+n])
-    return gram
-
-print(ngram(str.split(),2))
-print(ngram(str,2))
