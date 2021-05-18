44a04031612306056def16a864ff5c9d12ab867c
t-zetsu <74550103+t-zetsu@users.noreply.github.com>
2021-05-18 15:15:29 +0900

    Update 13.py

--- chapter2/13.py
+++ chapter2/13.py
@@ -6,7 +6,7 @@ with open("col2.txt", mode='r', encoding='utf-8') as f:
     col2 = f.readlines()
 
 for i in range(len(col1)):
-    output.append(col1[i].replace("\n","") + "\t" + col2[i] + "\n")
+    output.append(col1[i].replace("\n","") + "\t" + col2[i])
 
 with open("col.1-2.txt", mode='w', encoding='utf-8') as f:
     f.writelines(output)
